a58462e4252b42a00c4480e89b2bcd934cb2e154
Alexander Tretyakov <tpetyakov@gmail.com>
2021-12-26 19:20:41 +0300

    feat: add video section

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import './styles/index.scss';
 
-console.log('Hello world!');
+const inputRangeElements = document.querySelectorAll('.custom-input-range');
+
+for (const el of inputRangeElements) {
+  el.addEventListener('input', () => {
+    const value = el.value;
+    el.style.background = `linear-gradient(to right, #710707 0%, #710707 ${value}%, #c4c4c4 ${value}%, #c4c4c4 100%)`
+  })
+}
+
+ 
